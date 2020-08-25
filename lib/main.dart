3e7f8394dd5029ce1f0b20d0b604f3859daa7f64
mertodvp <merto-dvp@hotmail.com>
2020-08-25 21:51:30 +0300

    dependency update
    Signed-off-by: mertodvp <merto-dvp@hotmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,138 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'package:simple_permissions/simple_permissions.dart';
+import 'package:file_utils/file_utils.dart';
+import 'dart:math';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(Downloader());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Downloader extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    title: "File Downloader",
+    debugShowCheckedModeBanner: false,
+    home: FileDownloader(),
+    theme: ThemeData(primarySwatch: Colors.blue),
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class FileDownloader extends StatefulWidget {
+  @override
+  _FileDownloaderState createState() => _FileDownloaderState();
+}
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class _FileDownloaderState extends State<FileDownloader> {
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final imgUrl = "https://images6.alphacoders.com/683/thumb-1920-683023.jpg";
+  bool downloading = false;
+  var progress = "";
+  var path = "No Data";
+  var platformVersion = "Unknown";
+  Permission permission1 = Permission.WriteExternalStorage;
+  var _onPressed;
+  static final Random random = Random();
+  Directory externalDir;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter+=50;
-    });
+  void initState() {
+    super.initState();
+    downloadFile();
   }
 
+
+  Future<void> downloadFile() async {
+    Dio dio = Dio();
+    bool checkPermission1 =
+    await SimplePermissions.checkPermission(permission1);
+    // print(checkPermission1);
+    if (checkPermission1 == false) {
+      await SimplePermissions.requestPermission(permission1);
+      checkPermission1 = await SimplePermissions.checkPermission(permission1);
+    }
+    if (checkPermission1 == true) {
+      String dirloc = "";
+      if (Platform.isAndroid) {
+        dirloc = "/sdcard/download/";
+      } else {
+        dirloc = (await getApplicationDocumentsDirectory()).path;
+      }
+
+      var randid = random.nextInt(10000);
+
+      try {
+        FileUtils.mkdir([dirloc]);
+        await dio.download(imgUrl, dirloc + randid.toString() + ".jpg",
+            onReceiveProgress: (receivedBytes, totalBytes) {
+              setState(() {
+                downloading = true;
+                progress =
+                    ((receivedBytes / totalBytes) * 100).toStringAsFixed(0) + "%";
+              });
+            });
+      } catch (e) {
+        print(e);
+      }
+
+      setState(() {
+        downloading = false;
+        progress = "Download Completed.";
+        path = dirloc + randid.toString() + ".jpg";
+      });
+    } else {
+      setState(() {
+        progress = "Permission Denied!";
+        _onPressed = () {
+          downloadFile();
+        };
+      });
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+  Widget build(BuildContext context) => Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('File Downloader'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          child: downloading
+              ? Container(
+            height: 120.0,
+            width: 200.0,
+            child: Card(
+              color: Colors.black,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    'Downloading File: $progress',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+          )
+              : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(path),
+              MaterialButton(
+                child: Text('Request Permission Again.'),
+                onPressed: _onPressed,
+                disabledColor: Colors.blueGrey,
+                color: Colors.pink,
+                textColor: Colors.white,
+                height: 40.0,
+                minWidth: 100.0,
+              ),
+            ],
+          )));
 }
+
+enter image description here
